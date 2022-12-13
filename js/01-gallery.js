4cc6b768a94bf24090220f48eb37979da04f0f60
Ihor Poliakov <piv2003@gmail.com>
2022-12-13 23:20:54 +0200

    adding to 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,4 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryImg = document.querySelector('.gallery');
-// console.log(galleryItems);
+console.log(galleryItems);
+
+const createMarkupImg = galleryItems.map(
+    ({ original, preview, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+);
