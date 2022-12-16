cd85d58bb7eaa78c55782ff6673335d6ea9d2e72
Ihor Poliakov <piv2003@gmail.com>
2022-12-16 22:23:27 +0200

    Task-2 is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,8 +3,26 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryImgRef = document.querySelector('.gallery');
 const imgMarkup = createItemsMarkup(galleryItems);
-
 galleryImgRef.insertAdjacentHTML("beforeend", imgMarkup);
+//galleryImgRef.addEventListener("click", );
 
+function createItemsMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      </li>`;
+    })
+    .join("");
+}
 
-
+const ligthBox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
